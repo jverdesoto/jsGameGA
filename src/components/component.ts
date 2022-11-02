6a01ee7050ce6e4b82f9b9b7259c29767e49e9f0
Itziar <itziar.carrera@hotmail.com>
2022-11-02 21:27:56 +0100

    Clase 02/11/2022

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,11 +1,17 @@
+
 export class Component {
-	testMultiplyMethod(num1: number, num2: number): void {
+	testMultiplyMethod(num1: number, num2: number): void {  // Los parám. deben ser nºs. El método es VOID porque no devuelve nada.
 		// eslint-disable-next-line no-undef
 		console.log( num1 * num2 );
 	}
 
-	hello(): void {
+	hello(): void {                          // Tipo VOID porque no devuelve nada, no tiene un RETURN.
 		// eslint-disable-next-line no-undef
 		console.log('Hola!');
 	}
+	// Ejemplo de tipo STRING:
+	// 		hello(): string {                          // Tipo VOID porque no devuelve nada, no tiene un RETURN.
+	// 		// eslint-disable-next-line no-undef
+	// 		return 'Hola!';
+	// 		}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
+
 import './scss/styles.scss';
 import { Component } from './components/component';
+import axios from 'axios';  // Componente de Node que permite importar información. Es como FETCH, pero AXIOS permite configurar autenticaciones hacia una API y es más fácil.
 
-const component = new Component();
+const component: Component = new Component();  // Es tipo componente porque será un componente de la clase Componente
 component.hello();
 component.testMultiplyMethod(5,25);
+
+// let variable: any = '55';   // Evitar el tipo ANY a toda costa.
+
+// interface Vehiculos {
+// 	color: string,
+// 	marca: string,
+// 	year: number,
+// 	modelo: string,
+// 	puertas: number,
+// }
+// const vehiculos: Vehiculos = {
+// 	color: 'Azul',
+// 	marca: 'Fiat',
+// 	year: 1982,
+// 	modelo: 'Palio',
+// 	puertas: 5,
+// }
+// // eslint-disable-next-line no-undef
+// console.log(vehiculos);
+
+const url = 'https://jsonplaceholder.cypress.io/todos';
+
+axios.get(url).then(response => {  // Conecta con la API
+	const todo = response.data;
+	// eslint-disable-next-line no-undef
+	console.log(todo);
+});
+
+
+axios.get(url).then(response => {
+	const todo = response.data;
+	// eslint-disable-next-line no-undef
+	console.log(todo);
+	let index = 0;
+	for (const key in todo) {
+		if (todo[key].completed) {
+			index++
+			// eslint-disable-next-line no-undef
+			console.log(`%c ${index}.- ${todo[key].title}`, 'color: purple; font-weight:bold');  //%c significa que lo que le sigue tiene estilos.
+		}
+	}
+});
