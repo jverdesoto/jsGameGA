1fa0a033743f8bbcd1dadc6e8e82891bcb876592
Beatriz <bitrix24@hotmail.com>
2022-11-02 21:27:48 +0100

    draft

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import './scss/styles.scss';
 import { Component } from './components/component';
+import axios from 'axios';
 
 const component = new Component();
 component.hello();
 component.testMultiplyMethod(5,25);
+
+
+
+const vehiculos = {
+	marca: 'Fiat',
+	year: 1982,
+	color: 'Azul'
+}
+// eslint-disable-next-line no-undef
+console.log(vehiculos);
+
+const url = 'https://jsonplaceholder.cypress.io/todos';
+
+axios.get(url).then(response => {
+	const todo = response.data;
+	let index = 0;
+	for (const key in todo) {
+		if (todo[key].completed) {
+			index ++;
+			// eslint-disable-next-line no-undef
+			console.log(`%c ${index}.- ${todo[key].title}`, 'color:purple');
+		}
+	}
+
+});
+
