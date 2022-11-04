158cc2a1e6b7cc21748f435b163305db11aca9bc
aminusg <aino20m@gmail.com>
2022-11-04 19:45:57 +0100

    misnotas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,65 @@
 import './scss/styles.scss';
-import { Component } from './components/component';
+// import { Component } from './components/component';
+// import axios from 'axios';
 
-const component = new Component();
-component.hello();
-component.testMultiplyMethod(5,25);
+
+// const component = new Component();
+// component.hello();
+// component.testMultiplyMethod(5,25);
+
+// const vehiculos = {
+// 	marca: 'Fiat',
+// 	year: 1982,
+// 	color: 'azul'
+// }
+
+// // eslint-disable-next-line no-undef
+// console.log(vehiculos);
+// const url = 'https://jsonplaceholder.cypress.io/todos/1';
+
+// axios.get(url).then(response => {
+// 	const todo = response.data;
+// 	let index =0;
+// 	for (const key in todo){
+// 		if(todo[key].completed) {
+// 			index ++;
+// 			// eslint-disable-next-line no-undef
+// 			console.log(`%c ${index}.- ${todo[key].title}`, 'color:purple; font-weight:bold');
+
+// 		}
+// 	}
+// });
+//esLint-disable-next-Line no-undef
+document.getElementById('submit').addEventListener('click', (e) => {
+	e.preventDefault();
+	const titulo = document.getElementById('titulo');
+	const area = document.getElementById('area');
+	const resultado = document.getElementById('result');
+	let el;
+	let elTitulo;
+	let elContent;
+	let hr;
+	if (titulo.value === '' || area.value === '') {
+		alert('Por favor ingresa todos los campos');
+		return;
+	}
+
+	el= document.createElement('div');
+	el.classList.add('nota');
+
+	elTitulo = document.createElement('h3');
+	elTitulo.innerHTML = titulo.value;
+	el.appendChild(elTitulo);
+
+	elContent = document.createElement('div');
+	elContent.innerHTML = area.value;
+	el.appendChild(elContent);
+
+	hr = document.createElement('hr');
+	el.appendChild(hr);
+
+	resultado.prepend (el);
+	titulo.value = '';
+	area.value = '';
+
+});
